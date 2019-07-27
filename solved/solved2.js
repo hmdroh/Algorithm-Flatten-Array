ea34d43ed81a092f3a7dfa63de4325f0a6d52f53
Hamed Rohani <hamedrohani@MrRs-MBP.attlocal.net>
2019-07-26 21:45:37 -0700

    solved2

---
+++ solved/solved2.js
@@ -0,0 +1,7 @@
+function steamrollArray(arr) {
+    // I'm a steamroller, baby
+    var flat = [].concat(...arr);
+    return flat.some(Array.isArray) ? steamrollArray(flat) : flat;
+  }
+  
+  steamrollArray([1, [2], [3, [[4]]]]);
